65be4a5d45ed7cf4ade9daffa726ae5d78b1144b
Jorge Figueroa <yknx.4.b@gmail.com>
2016-11-25 15:22:58 -0600

    Fix date format in Post

--- src/components/IndexPost.jsx
+++ src/components/IndexPost.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import PureComponent from './PureComponent'
 import readingTime from 'reading-time'
+import {formatDate} from '../code/Utils'
 
 class Post extends PureComponent {
   render() {
     const post = this.props.post;
     const post_attributes = post.attributes;
+    console.log(post_attributes);
+    console.log(new Date(post_attributes['inserted-at']));
     return (
       <article>
         <h1 className="article-title"><a href={post.links.self}>{post_attributes.title}</a></h1>
         <p className="article-info">
-          on <strong>{post_attributes.inserted_at}</strong> by <strong>Placeholder Author</strong>
+          on <strong>{formatDate(post_attributes['inserted-at'])}</strong> by <strong>Placeholder Author</strong>
           &middot;
           <span className="f-post-tags"><a href="/tags"><i className="fa fa-tag"></i></a>
             { post_attributes['tag-ids'].map( tag_id =>
-              <a key={tag_id} href={`/tags/${tag_id}`}>{tag_id}</a>,
+              <a key={tag_id} href={`/tags/${tag_id}`}>{tag_id}, </a>
             )}
           </span>
           &middot; <strong>{ readingTime(post_attributes.body).text } reading time</strong>
