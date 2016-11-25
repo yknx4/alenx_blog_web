52f98d79b4baf149f87e5f02d286d42cc149b853
Jorge Figueroa <yknx.4.b@gmail.com>
2016-11-25 15:22:41 -0600

    Create Utils module

---
+++ src/code/Utils.js
@@ -0,0 +1,19 @@
+
+function formatDate(date) {
+  date = new Date(date);
+  const options = {
+    weekday: "long", year: "numeric", month: "short",
+    day: "numeric"
+  };
+
+  return date.toLocaleDateString(navigatorLanguage(), options);
+}
+
+function navigatorLanguage() {
+  // eslint-disable-next-line
+  return navigator.languages && navigator.languages[0] || // Chrome / Firefox
+         navigator.language ||   // All browsers
+         navigator.userLanguage; // IE <= 10
+}
+
+export {formatDate, navigatorLanguage}
