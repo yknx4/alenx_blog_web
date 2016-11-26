4fa6a3eef58501e985a977f290c55bded94ff8a2
Ale Figueroa <yknx.4.b@gmail.com>
2016-11-26 10:01:36 -0600

    Extend Purecomponent to dynamically define properties

--- src/components/PureComponent.jsx
+++ src/components/PureComponent.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import _ from 'lodash'
 
 class PureComponent extends React.Component {
   constructor(props) {
     super(props);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  fetch(propName, defaultValue="") {
+    return this.props[propName] || defaultValue;
+  }
+
+  property(name, defaultValue="") {
+    Object.defineProperty(this, name, { get: function () { return this.fetch(name, defaultValue) } });
+  }
+
+  properties(properties) {
+    _.forEach(properties, (value, key) => {
+      this.property(key, value);
+    });
   }
 }
 
---
+++ src/components/PureComponent.test.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+chai.use(chaiImmutable);
+
+import PureComponent from './PureComponent';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag
+} from 'react-addons-test-utils';
+
+it('fetches a prop', () => {
+  const component = new PureComponent({awesome: "me!"});
+  expect(component.fetch("awesome")).to.equal("me!");
+  expect(component.fetch("invalidWithoutDefault")).to.equal("");
+  expect(component.fetch("invalidWithDefault", 1)).to.equal(1);
+});
+
+it('defines a property', () => {
+  const component = new PureComponent({awesome: "me!"});
+  component.property('awesome');
+  component.property('noob', "you!");
+
+  expect(component.awesome).to.equal("me!");
+  expect(component.noob).to.equal("you!");
+});
+
+it('defines many properties', () => {
+  const component = new PureComponent({awesome: "me!", noob: "you!"});
+  component.properties({
+    awesome: "",
+    noob: "",
+    invalid: 1
+  });
+
+  expect(component.awesome).to.equal("me!");
+  expect(component.noob).to.equal("you!");
+  expect(component.invalid).to.equal(1);
+});
