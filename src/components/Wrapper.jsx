a6605f8501c33f2147566b09013cc174b3a5fbff
Ale Figueroa <yknx.4.b@gmail.com>
2016-11-27 23:12:46 -0600

    Use span for wrapper

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import PureComponent from './PureComponent'
 
+// Waiting for https://github.com/facebook/react/issues/2127 to be closed
 function Wrapper(props) {
   return (
-    <div>
+    <span>
       {props.children}
-    </div>
+    </span>
   );
 }
 
